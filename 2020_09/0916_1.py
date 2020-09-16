380024e71deb7c444fe159a16c82f952fb97424e
ChoSanghyuk <59812791+ChoSanghyuk@users.noreply.github.com>
2020-09-16 14:22:52 +0900

    200916

---
+++ 2020_09/0916_1.py
@@ -0,0 +1,11 @@
+# 정수삼각형 (프로그래머스)
+def solution(triangle):
+    for i in range(1,len(triangle)):
+        for j in range(len(triangle[i])):
+            if j ==0:
+                triangle[i][j] += triangle[i-1][0]
+            elif j == len(triangle[i])-1:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+    return max(triangle[-1])
