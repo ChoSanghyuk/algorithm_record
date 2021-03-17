37860ebbec850c1183f82e19da19ee94ce49010a
ChoSanghyuk <59812791+ChoSanghyuk@users.noreply.github.com>
2021-03-17 17:01:17 +0900

    Create 210317.java

---
+++ 2021_03/210317.java
@@ -0,0 +1,21 @@
+// 프로그래머스 두 개 뽑아서 더하기
+import java.util.Set;
+import java.util.TreeSet;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> sets = new TreeSet<>();
+        for(int i =0; i< numbers.length-1; i++){
+            for(int j = i+1 ; j < numbers.length; j++){
+                sets.add( numbers[i] + numbers[j]);
+            }
+        }
+        int[] answers = new int[sets.size()];
+        int k=0;
+        for(Integer i : sets){
+            answers[k++] = i;
+        }
+        return answers;
+    }
+    
+}
